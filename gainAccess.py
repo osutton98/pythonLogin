90ace0140d2d97cd4247e02733c09f07052f772a
osutton98 <owsutton31@gmail.com>
2020-02-09 00:37:49 -0500

    Goals of the Project

---
+++ gainAccess.py
@@ -0,0 +1,12 @@
+'''The Purpose of this Program is to implement the simple concepts learned in by Python Basics bootcamp course.
+        This Program will consist of asking a user to create a username, password. then prompt them  and ask
+        if they would like to login to the program if so it will ask for credentials, they are allowed to try 3 times before
+         the program will end.
+
+         1. Stretch goals will include email capture and sending email if correct security questions are answered
+
+         2. GUI
+
+         3. Two factor authentication with Twillio
+
+         '''
